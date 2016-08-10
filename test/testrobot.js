f9cb49c56f3f9cc3eb5f7a446e32a50b97fed06c
adhitya ramadhanus <adhitya.ramadhanus@xtremax.com>
2016-08-10 16:01:13 +0700

    add more testcase

--- test/testrobot.js
+++ test/testrobot.js
@@ -17,4 +17,81 @@ describe('Tic-tac-toe Bot', function () {
 		computerMove.col.should.equal(1)
 		done()
   })
+  it('Test on state 2', function (done) {
+    var state = [ 
+			['o', 'o', 'o'],
+			['o', 'o', 'o'],
+			['o', 'o', 'o']
+		]
+		var computerMove = ticbot(state)
+		computerMove.row.should.equal(-1)
+		computerMove.col.should.equal(-1)
+		done()
+  })
+  it('Test on state 3', function (done) {
+    var state = [ 
+			['x', 'x', ''],
+			['', '', ''],
+			['', '', '']
+		]
+		var computerMove = ticbot(state)
+		computerMove.row.should.equal(0)
+		computerMove.col.should.equal(2)
+		done()
+  })
+  it('Test on state 4', function (done) {
+    var state = [ 
+			['x', '', ''],
+			['', '', ''],
+			['', '', 'x']
+		]
+		var computerMove = ticbot(state)
+		computerMove.row.should.equal(1)
+		computerMove.col.should.equal(1)
+		done()
+  })
+  it('Test on state 5', function (done) {
+    var state = [ 
+			['', '', ''],
+			['', 'x', ''],
+			['', 'x', '']
+		]
+		var computerMove = ticbot(state)
+		computerMove.row.should.equal(0)
+		computerMove.col.should.equal(1)
+		done()
+  })
+  it('Test on state 6', function (done) {
+    var state = [ 
+			['x', 'x', 'o'],
+			['', '', 'o'],
+			['', '', '']
+		]
+		var computerMove = ticbot(state)
+		computerMove.row.should.equal(2)
+		computerMove.col.should.equal(2)
+		done()
+  })
+  it('Test on state 7', function (done) {
+    var state = [ 
+			['x', '', ''],
+			['', 'o', ''],
+			['o', '', 'x']
+		]
+		var computerMove = ticbot(state)
+		computerMove.row.should.equal(0)
+		computerMove.col.should.equal(2)
+		done()
+  })
+  it('Test on state 8', function (done) {
+    var state = [ 
+			['', 'o', 'o'],
+			['', 'x', ''],
+			['', 'x', '']
+		]
+		var computerMove = ticbot(state)
+		computerMove.row.should.equal(0)
+		computerMove.col.should.equal(0)
+		done()
+  })
 })
